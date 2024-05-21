967170f47ab5e7c1b467a191251b0aaaaa0f359d
SnowBall512 <huong05hn1203@gmail.com>
2024-05-21 10:52:50 +0700

    add

--- app/src/main/java/com/example/btth04/MainActivity.java
+++ app/src/main/java/com/example/btth04/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.btth04;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -11,12 +13,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.sql.SQLData;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText edtMa, edtTen, edtSo;
     Button btnIn, btnDel, btnUp, btnQue;
+    ListView list;
+    ArrayList<String> myList;
+    ArrayAdapter<String> myAdapter;
+    SQLiteDatabase mydb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +34,12 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        ListView list;
-        ArrayList
+        edtMa=findViewById(R.id.edtMa);
+        edtTen=findViewById(R.id.edtTen);
+        edtSo=findViewById(R.id.edtSo);
+        btnDel = findViewById(R.id.btnDel);
+        btnIn = findViewById(R.id.btnInsert);
+        btnQue=findViewById(R.id.btnQuery);
+        btnUp=findViewById(R.id.btnUpdate);
     }
 }
