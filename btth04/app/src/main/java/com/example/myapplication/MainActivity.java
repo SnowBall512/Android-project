970b548ebd42950da3ac2cb0b05198878e82a934
SnowBall512 <huong05hn1203@gmail.com>
2024-05-21 22:28:43 +0700

    add

--- btth04/app/src/main/java/com/example/myapplication/MainActivity.java
+++ btth04/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -23,11 +23,12 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     EditText edtMa, edtTen, edtSo;
-    Button btnIn, btnDel, btnUp, btnQue;
+    Button btnIn, btnDel, btnUp;
     ListView list;
     ArrayList<String> myList;
     ArrayAdapter<String> myAdapter;
     SQLiteDatabase mydb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,73 +39,80 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        edtMa=findViewById(R.id.edtMa);
-        edtTen=findViewById(R.id.edtTen);
-        edtSo=findViewById(R.id.edtSo);
+        edtMa = findViewById(R.id.edtMa);
+        edtTen = findViewById(R.id.edtTen);
+        edtSo = findViewById(R.id.edtSo);
         btnDel = findViewById(R.id.btnDel);
         btnIn = findViewById(R.id.btnInsert);
-        btnQue=findViewById(R.id.btnQuery);
-        btnUp=findViewById(R.id.btnUpdate);
-        list= findViewById(R.id.list);
+        btnUp = findViewById(R.id.btnUpdate);
+        list = findViewById(R.id.list);
         myList = new ArrayList<>();
-        myAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,myList);;
+        myAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, myList);
         list.setAdapter(myAdapter);
-        mydb = openOrCreateDatabase("qlsv.db",MODE_PRIVATE, null);
+        mydb = openOrCreateDatabase("qlsv.db", MODE_PRIVATE, null);
         try {
-            String sql="Create table tblop(malop TEXT primary key, tenlop TEXT,siso INTERGER)";
+            String sql = "Create table tblop(malop TEXT primary key, tenlop TEXT, siso INTEGER)";
             mydb.execSQL(sql);
+        } catch (Exception e) {
+            Log.e("Error", "Table đã tồn tại");
         }
-        catch (Exception e){
-            Log.e("Error","Table đã tồn tại");
-        }
+
+        edtMa.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    String maLop = edtMa.getText().toString();
+                    if (!maLop.isEmpty()) {
+                        displayClassInfo(maLop);
+                    }
+                }
+            }
+        });
+
         btnIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String maLop =edtMa.getText().toString();
-                String tenLop=edtTen.getText().toString();
-                int siso = Integer.parseInt(edtSo.getText().toString());
-                ContentValues myVl=new ContentValues();
-                myVl.put("malop",maLop);
-                myVl.put("tenlop",tenLop);
-                myVl.put("siso",siso);
-                String msg="";
-                if(mydb.insert("tblop", null, myVl)==1){
-                    msg="Thêm không thành công ";
-                }
-                else {
-                    msg="Thêm thành công";
-                }
-                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
-            }
-        });
-        btnQue.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                myList.clear();;
-                Cursor c=mydb.query("tblop", null, null, null,null, null,null);
-                c.moveToNext();
-                String data="";
-                while(c.isAfterLast()==false){
-                    data=c.getString(0)+" - "+c.getString(1)+" -"+c.getString(2);
-                    c.moveToNext();
-                    myList.add(data);
+                String maLop = edtMa.getText().toString();
+                String tenLop = edtTen.getText().toString();
+                String sisoStr = edtSo.getText().toString();
 
+                if (!isNumeric(sisoStr)) {
+                    Toast.makeText(MainActivity.this, "Sĩ số phải là một số", Toast.LENGTH_SHORT).show();
+                    return;
                 }
-                c.close();
-                myAdapter.notifyDataSetChanged();
+
+                int siso = Integer.parseInt(sisoStr);
+
+                if (checkMalopExists(maLop)) {
+                    Toast.makeText(MainActivity.this, "Mã lớp đã tồn tại", Toast.LENGTH_SHORT).show();
+                } else {
+                    ContentValues myVl = new ContentValues();
+                    myVl.put("malop", maLop);
+                    myVl.put("tenlop", tenLop);
+                    myVl.put("siso", siso);
+
+                    long result = mydb.insert("tblop", null, myVl);
+                    if (result == -1) {
+                        Toast.makeText(MainActivity.this, "Thêm không thành công", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Thêm thành công", Toast.LENGTH_SHORT).show();
+                        refreshList(); // Refresh the list view
+                    }
+                }
             }
         });
+
         btnDel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String maLop= edtMa.getText().toString();
-                int n = mydb.delete("tblop", "malop = ?", new String[] {maLop});
-                String msg="";
-                if(n==0){
-                    msg="Không có gì để xóa";
-                }
-                else {
-                    msg= n+ "Xóa thành công";
+                String maLop = edtMa.getText().toString();
+                int n = mydb.delete("tblop", "malop = ?", new String[]{maLop});
+                String msg = "";
+                if (n == 0) {
+                    msg = "Không có gì để xóa";
+                } else {
+                    msg = n + " Xóa thành công";
+                    refreshList(); // Refresh the list view
                 }
                 Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
             }
@@ -113,20 +121,76 @@ public class MainActivity extends AppCompatActivity {
         btnUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int siso = Integer.parseInt(edtSo.getText().toString());
+                String sisoStr = edtSo.getText().toString();
+
+                if (!isNumeric(sisoStr)) {
+                    Toast.makeText(MainActivity.this, "Sĩ số phải là một số", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                int siso = Integer.parseInt(sisoStr);
                 String malop = edtMa.getText().toString();
                 ContentValues myVl = new ContentValues();
                 myVl.put("siso", siso);
-                int n= mydb.update("tblop", myVl, "malop =?", new String[]{malop});
-                String msg="";
-                if(n==0){
-                    msg="Cập nhật không thành công";
-                }
-                else {
-                    msg=n+"Cập nhật thành công";
+                int n = mydb.update("tblop", myVl, "malop = ?", new String[]{malop});
+                String msg = "";
+                if (n == 0) {
+                    msg = "Cập nhật không thành công";
+                } else {
+                    msg = n + " Cập nhật thành công";
+                    refreshList(); // Refresh the list view
                 }
                 Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
             }
         });
+
+        refreshList();
+    }
+
+    private void refreshList() {
+        myList.clear();
+        Cursor c = mydb.query("tblop", null, null, null, null, null, null);
+        c.moveToNext();
+        String data = "";
+        while (!c.isAfterLast()) {
+            data = c.getString(0) + " - " + c.getString(1) + " - " + c.getString(2);
+            c.moveToNext();
+            myList.add(data);
+        }
+        c.close();
+        myAdapter.notifyDataSetChanged();
+    }
+
+    private void displayClassInfo(String malop) {
+        Cursor cursor = mydb.rawQuery("SELECT * FROM tblop WHERE malop=?", new String[]{malop});
+        if (cursor.moveToFirst()) {
+            String tenLop = cursor.getString(cursor.getColumnIndexOrThrow("tenlop"));
+            int siso = cursor.getInt(cursor.getColumnIndexOrThrow("siso"));
+            edtTen.setText(tenLop);
+            edtSo.setText(String.valueOf(siso));
+        } else {
+            edtTen.setText("");
+            edtSo.setText("");
+        }
+        cursor.close();
+    }
+
+    private boolean checkMalopExists(String malop) {
+        Cursor cursor = mydb.rawQuery("SELECT * FROM tblop WHERE malop=?", new String[]{malop});
+        boolean exists = (cursor.getCount() > 0);
+        cursor.close();
+        return exists;
+    }
+
+    private boolean isNumeric(String str) {
+        if (str == null) {
+            return false;
+        }
+        try {
+            double d = Double.parseDouble(str);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
     }
 }
