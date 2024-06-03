681db5af0789d0fab9f46fca3954519c41e7a931
huongnguyen-512 <huong05hn1203@gmail.com>
2024-06-03 15:44:25 +0700

    Xu ly su kien Camera Activity

--- BTTH2-Intent2/app/src/main/java/com/example/btth2_intent2/CameraActivity.java
+++ BTTH2-Intent2/app/src/main/java/com/example/btth2_intent2/CameraActivity.java
@@ -1,15 +1,28 @@
 package com.example.btth2_intent2;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import static android.provider.MediaStore.ACTION_IMAGE_CAPTURE;
 
 public class CameraActivity extends AppCompatActivity {
+
+    ImageView img;
+    ImageButton btncam;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +34,27 @@ public class CameraActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        img=findViewById(R.id.imgcam);
+        btncam=findViewById(R.id.btncam);
+        btncam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent icam = new Intent(ACTION_IMAGE_CAPTURE);
+                if (ActivityCompat.checkSelfPermission(CameraActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(CameraActivity.this, new String[]{Manifest.permission.CAMERA},1);
+                    return;
+                }
+                startActivityForResult(icam,99);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==99 && resultCode== Activity.RESULT_OK){
+            Bitmap photo=(Bitmap) data.getExtras().get("data");
+            img.setImageBitmap(photo);
+        }
     }
 }
