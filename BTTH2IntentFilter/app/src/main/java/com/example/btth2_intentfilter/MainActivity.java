fa0d6a87e3b47e2b590ba91d404c41184a33f348
huongnguyen-512 <huong05hn1203@gmail.com>
2024-06-03 18:52:56 +0700

    Giao dien BTTH2-Intent Filter

---
+++ BTTH2IntentFilter/app/src/main/java/com/example/btth2_intentfilter/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.btth2_intentfilter;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnshow;
+    EditText edt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        edt = findViewById(R.id.editText);
+        btnshow = findViewById(R.id.btnweb);
+        btnshow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iweb = new Intent(Intent.ACTION_VIEW, Uri.parse("https://"+edt.getText().toString()))
+            }
+        });
+    }
+}
